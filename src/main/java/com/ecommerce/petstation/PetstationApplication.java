19e67fc4025112ad8c318a7ae09d22e6a07da03a
Gabryel Leite <gabryeleite@gmail.com>
2024-08-25 10:28:27 -0300

    first commit

---
+++ src/main/java/com/ecommerce/petstation/PetstationApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.petstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstationApplication.class, args);
+	}
+
+}
