d5e23410115d45ab1b0f110a6f3f2fb388e4107a
Gabryel Leite <gabryeleite@gmail.com>
2024-09-16 18:19:28 -0300

    update PedidoProdutoRepository

--- src/main/java/com/ecommerce/petstation/repositories/PedidoProdutoRepository.java
+++ src/main/java/com/ecommerce/petstation/repositories/PedidoProdutoRepository.java
@@ -7,19 +7,20 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ecommerce.petstation.models.Carrinho;
+import com.ecommerce.petstation.models.PedidoProduto;
 
 @Repository
-public interface CarrinhoRepository extends JpaRepository<Carrinho, Integer> {
+public interface PedidoProdutoRepository extends JpaRepository<PedidoProduto, Integer> {
     
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO compra_item (num_pedido, num_produto, qnt_produto)" +
-                   " VALUES (:num_pedido, :num_produto, :qnt_produto)", nativeQuery = true)
+    @Query(value = "INSERT INTO compra_item (num_pedido, num_produto, qnt_produto, preco_produto)" +
+                   " VALUES (:num_pedido, :num_produto, :qnt_produto, :preco_produto)", nativeQuery = true)
     void createCarrinho(
         @Param("num_pedido") Integer numPedido,
         @Param("num_produto") Integer numProduto,
-        @Param("qnt_produto") Integer qntProduto
+        @Param("qnt_produto") Integer qntProduto,
+        @Param("preco_produto") Integer precoProduto
     );
 
 }
