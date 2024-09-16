1519c993a8b1aee39cfc63c32ad1eb73f28e3de6
Gabryel Leite <gabryeleite@gmail.com>
2024-09-16 17:24:00 -0300

    update model Categoria

--- src/main/java/com/ecommerce/petstation/models/Categoria.java
+++ src/main/java/com/ecommerce/petstation/models/Categoria.java
@@ -1,30 +1,36 @@
 package com.ecommerce.petstation.models;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "categoria", schema = "petstation", 
-    uniqueConstraints = {@UniqueConstraint(columnNames = "nome")})
+@Table(name = "categoria", uniqueConstraints = @UniqueConstraint(columnNames = "nome"))
 public class Categoria {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_seq")
-    @SequenceGenerator(name = "categoria_seq", schema = "petstation", 
-    sequenceName = "categoria_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categoria_id_seq")
+    @SequenceGenerator(name = "categoria_id_seq", sequenceName = "petstation.categoria_id_seq", allocationSize = 1)
     @Column(name = "id_categoria")
-    private Integer idCategoria; 
+    private Integer idCategoria;
 
-    @Column(name = "nome", nullable = false, length = 50)
+    @Column(nullable = false, length = 50)
     private String nome;
 
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Subcategoria> subcategorias;
+
     // Getters and Setters
+
     public Integer getIdCategoria() {
         return idCategoria;
     }
@@ -40,6 +46,15 @@ public class Categoria {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public List<Subcategoria> getSubcategorias() {
+        return subcategorias;
+    }
+
+    public void setSubcategorias(List<Subcategoria> subcategorias) {
+        this.subcategorias = subcategorias;
+    }
+
 }
 
 /*              Scprit SQL
